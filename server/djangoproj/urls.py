f60599b87f8df3045e05fe5b64ebd7288469d508
ningyuting7-boop <ningyuting7@gmail.com>
2025-10-15 17:42:29 -0700

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
